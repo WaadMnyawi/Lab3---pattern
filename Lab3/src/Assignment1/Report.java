c85609ec4ad08a21ffa93a8517a231fba552b099
waadl <waadl@DESKTOP-CBTJSG5>
2024-03-04 23:14:36 +0300

    Report class - Assignment1

---
+++ Lab3/src/Assignment1/Report.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Assignment1;
+
+/**
+ *
+ * @author waadl
+ */
+ public class Report implements Document { 
+  
+    @Override 
+    public Document clone() { 
+       return new Report(); 
+    } 
+  
+   @Override 
+    public void edit() { 
+        System.out.println("Editing report..."); 
+     }
+ } 
