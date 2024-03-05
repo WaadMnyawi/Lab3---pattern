bb9c8569aea32ed11c3b92409665d2fb9502eb21
waadl <waadl@DESKTOP-CBTJSG5>
2024-03-05 09:41:18 +0300

    Publishing System class - Assignment1

---
+++ Lab3/src/Assignment1/PublishingSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Assignment1;
+
+/**
+ *
+ * @author waadl
+ */
+public class PublishingSystem {
+    
+    public static void main(String[] args) { 
+       Document article = new Article(); 
+    
+       Document editedArticle = article.clone(); 
+       
+       editedArticle.edit(); 
+      } 
+ } 
